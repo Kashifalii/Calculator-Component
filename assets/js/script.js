548c28ad9ff8e27eeb63a093e40a1767aef247b1
Kashif Ali <136927508+Kashifalii@users.noreply.github.com>
2024-09-15 11:59:35 +0500

    updated

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// changing active class in navbar
+// const activeClass = () => {
+//     const links = document.querySelectorAll(".nav-link");
+
+//     links.forEach(link => {
+//         links.addEventListener('click', () => {
+//             links.forEach(link => {
+//             });
+//             alert("Click");
+//         });
+//     });
+// };
